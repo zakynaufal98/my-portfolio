b07b3cd50ea22e59a9c7597220fa30207e820db9
zakynaufal98 <zakynaufal1998@gmail.com>
2022-09-14 11:52:18 +0700

    update some feature

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -2,21 +2,29 @@ import profile from "../assets/4x6.jpg";
 import program from "../assets/program.jpg";
 import epicclone from "../assets/epic-clone.png";
 import netflixclone from "../assets/netflix-clone.png";
-import { SocialIcon } from "react-social-icons";
+// import { SocialIcon } from "react-social-icons";
 import htmlLogo from "../assets/html5.png";
 import cssLogo from "../assets/css.png";
 import jsLogo from "../assets/Js.png";
 import sql from "../assets/png-sql.png";
 import reactLogo from "../assets/React-icon.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Contents = () => {
   return (
-    <div className="page-content text-white ">
+    <div className="page-content text-white min-w-[600px]">
       <div className="home flex bg-[#16324F] items-center pr-8 font-small justify-between text-2xl gap-8">
         <img
           src={program}
           alt=""
-          className="w-[60%] h-[35rem] rounded-r-[10rem] object-cover "
+          className="w-[65%] h-[40rem] rounded-r-[18rem]  object-cover"
         />
         <div>Someone Interested in Frontend Developer</div>
       </div>
@@ -24,12 +32,12 @@ const Contents = () => {
         <div className="text-xl font-medium mb-2">About Me</div>
         <img
           src={profile}
-          alt="profile-image"
+          alt="profile"
           className="w-[150px] h-[150px] rounded-full mb-4 object-cover"
         />
-        <h1>Zaky Naufal</h1>
-        <h1>Frontend Developer</h1>
-        <div className="flex w-[360px] mt-3 mb-3 items-center">
+        <h1 className="text-xl">Zaky Naufal</h1>
+        <h1 className="text-l">Frontend Developer</h1>
+        <div className="flex w-[360px] mt-3 mb-3 items-center text-justify text-xs">
           A self-taught person who likes to learn about web programming through
           online or offline training. Love to learn new things arround
           programming and other
@@ -37,71 +45,109 @@ const Contents = () => {
       </div>
       <div className="skillset  bg-[#16324F] flex items-center flex-col p-4">
         <span>Skillset</span>
-        <ul className="flex justify-center gap-5 mt-2">
+        <ul className="flex justify-center gap-2 mt-2 items-center">
           <li>
-            <img src={htmlLogo} alt="html" className="w-20" />
+            <img src={htmlLogo} alt="html" className="h-18 w-16" />
           </li>
           <li>
-            <img src={cssLogo} alt="css" className="w-20" />
+            <img src={cssLogo} alt="css" className="h-18 w-16" />
           </li>
           <li>
-            <img src={jsLogo} alt="js" className="w-20" />
+            <img src={jsLogo} alt="js" className="h-18 w-16" />
           </li>
           <li>
-            <img src={sql} alt="sql" className="max-w-20 " />
+            <img src={sql} alt="sql" className=" h-18 w-16 " />
           </li>
           <li>
-            <img src={reactLogo} alt="react" className="w-20" />
+            <img src={reactLogo} alt="react" className="h-18 w-16" />
           </li>
         </ul>
       </div>
-      <div className="myproject flex justify-center bg-[#13293D] p-6 gap-5 ">
-        <a href="https://152235865100788-dts-final-project.netlify.app/">
-          <img
-            src={epicclone}
-            alt="epic-clone"
-            className="w-[25rem] hover:brightness-75"
-          />
-        </a>
-        <a href="https://pair-03a-dts-mini-project.netlify.app/" className="">
-          <img
-            src={netflixclone}
-            alt="netflix-clone"
-            className="w-[25rem] hover:brightness-75 "
-          />
-        </a>
+      <div className="bg-[#13293D] flex flex-col items-center p-4">
+        <span>My Project</span>
+
+        <div className="myproject flex justify-center p-6 gap-5 ">
+          <a href="https://152235865100788-dts-final-project.netlify.app/">
+            <img
+              src={epicclone}
+              alt="epic-clone"
+              className="w-[25rem] hover:brightness-75"
+            />
+          </a>
+          <a href="https://pair-03a-dts-mini-project.netlify.app/" className="">
+            <img
+              src={netflixclone}
+              alt="netflix-clone"
+              className="w-[25rem] hover:brightness-75 "
+            />
+          </a>
+        </div>
       </div>
+
       <div className="contact bg-[#16324F] flex flex-col items-center p-4">
         <span>Social Media</span>
+
         <p className="mt-2">Find me in :</p>
         <ul className="flex gap-2 mt-4">
           <li>
-            <SocialIcon
-              url="https://instagram.com/zaky_naufal"
-              bgColor="white"
-              style={{ height: 40, width: 40 }}
-            />
+            <a
+              href="https://instagram.com/zaky_naufal"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faInstagram}
+                className="hover:text-blue-500 text-[2rem]"
+              />
+            </a>
           </li>
           <li>
-            <SocialIcon
-              url="https://facebook.com/zakynaufal1998"
-              bgColor="white"
-              style={{ height: 40, width: 40 }}
-            />
+            <a
+              href="https://facebook.com/zakynaufal1998"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faFacebook}
+                className="hover:text-blue-500 text-[2rem]"
+              />
+            </a>
           </li>
           <li>
-            <SocialIcon
-              url="https://twitter.com/zaky_naufal"
-              bgColor="white"
-              style={{ height: 40, width: 40 }}
-            />
+            <a
+              href="https://twitter.com/zaky_naufal"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faTwitter}
+                className="hover:text-blue-500 text-[2rem]"
+              />
+            </a>
           </li>
           <li>
-            <SocialIcon
-              url="https://linkedin.com/zaky_naufal"
-              bgColor="white"
-              style={{ height: 40, width: 40 }}
-            />
+            <a
+              href="https://linkedin.com/zaky_naufal"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                className="hover:text-blue-500 text-[2rem]"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/zakynaufal98"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faGithub}
+                className="hover:text-blue-500 text-[2rem]"
+              />
+            </a>
           </li>
         </ul>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 const Navbar = () => {
   return (
-    <div className="nav flex bg-gradient-to-r from-[#18435A] t text-white justify-between h-12 items-center pr-6 pl-6 fixed w-full">
-      <div className="left-nav text-blue-400 text-l font-bold">
+    <div className="nav flex bg-[#18435A] text-white justify-between h-12 items-center pr-6 pl-6 sticky top-0 w-full min-w-[600px] z-10">
+      <div className="left-nav text-blue-400 text-l font-bold hover:text-white">
         <h3>Zaky Web Page</h3>
       </div>
       <div className="right-nav flex">
         <ul className="gap-3 text-sm hidden md:flex">
-          <li>Home</li>
-          <li>About</li>
-          <li>Skill</li>
-          <li>My Project</li>
-          <li>Contact</li>
+          <li className="hover:text-blue-500">Home</li>
+          <li className="hover:text-blue-500">About</li>
+          <li className="hover:text-blue-500">Skill</li>
+          <li className="hover:text-blue-500">My Project</li>
+          <li className="hover:text-blue-500">Contact</li>
         </ul>
       </div>
     </div>
